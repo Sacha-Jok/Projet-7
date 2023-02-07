fdd458fa435cfc1ec9aaa3a61a3e8b3dfce693fa
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-07 10:03:17 +0100

    dropdown arrow animation

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react';
 import arrow from '../../assets/images/upArrow.svg';
 import styles from "./styles.module.scss";
 
-const Dropdown = ({title, text}) => (
-    <details className={styles.__details} >
-        <summary className={styles.__title}>{title}<img id='arrow' src={arrow} alt=" Flèche" /></summary>
+const Dropdown = ({title, text}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <details className={styles.__details} open={isOpen} onToggle={() => setIsOpen(!isOpen)}>
+        <summary className={styles.__title}>
+            {title}
+            <img id='arrow' src={arrow} alt=" Flèche" style={{
+                transform: `rotate(${isOpen ? 180 : 0}deg)`,
+                transition: "transform 0.5s ease"
+                }} />
+        </summary>
         <p className={styles.__description}>{text}</p>
     </details>
-);
+  );
+};
 
 export default Dropdown;
