35e92dc84f87b3eba5a750d3f5df506216cb906c
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-09 20:00:30 +0100

    add card to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import styles from "./styles.module.scss";
 
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import Banner from '../../components/Banner';
+import Card from '../../components/Card';
 
 import bannerImg from '../../assets/images/homeBanner.png';
-
-
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Banner banner={bannerImg} text='Chez vous, partout et ailleurs' />
+            <div className={styles.__cardContainer}><Card /></div>
             <Footer />
         </div>
     );
