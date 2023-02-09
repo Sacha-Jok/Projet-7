97720c90de2293d3f7c2be902e7ae58ca632f2e3
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-09 04:26:31 +0100

    home card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom';
-
+import Rentals from '../../datas/logements.json'
 import styles from "./styles.module.scss";
 
 const Card = () => {
-  
+    return (
+        Rentals.map(rentalInfo =>
+            <Link className={styles.__card} key={rentalInfo.id} to={`../logements/${rentalInfo.id}`} >
+                    <img className={styles.__cover} src={rentalInfo.cover} alt={`Photo of  + ${rentalInfo.title}`} />
+                    <h2 className={styles.__title}>{rentalInfo.title}</h2>
+            </Link>
+        )
+    );
 };
 
 export default Card;
