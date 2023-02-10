3e119b8028738ed018591d63fe04e9ccb5e14599
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-10 02:14:45 +0100

    dorpdown correction

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import Dropdown from '../../components/Dropdown';
 import Banner from '../../components/Banner';
 
 import {aboutTxt} from '../../datas/constants';
+import styles from "./styles.module.scss";
 import bannerImg from '../../assets/images/aboutBanner.png';
 
 
@@ -15,10 +17,12 @@ const About = () => {
         <div>
             <Navbar />
             <Banner banner={bannerImg} />
-            <Dropdown title={aboutTxt.first.title} text={aboutTxt.first.text} />
-            <Dropdown title={aboutTxt.second.title} text={aboutTxt.second.text} />
-            <Dropdown title={aboutTxt.third.title} text={aboutTxt.third.text} />
-            <Dropdown title={aboutTxt.fourth.title} text={aboutTxt.fourth.text} />
+            <div className={styles.__dropdownContainer}>
+                <Dropdown title={aboutTxt.first.title} text={aboutTxt.first.text} />
+                <Dropdown title={aboutTxt.second.title} text={aboutTxt.second.text} />
+                <Dropdown title={aboutTxt.third.title} text={aboutTxt.third.text} />
+                <Dropdown title={aboutTxt.fourth.title} text={aboutTxt.fourth.text} />
+            </div>
             <Footer />
         </div>
     );
