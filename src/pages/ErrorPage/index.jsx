526c4519385bddfcd8bbce2ad8f1d1732f1208a9
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-10 05:12:46 +0100

    change error page

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
-import styles from "../../styles/pages/error-page.module.scss"
+import styles from "./styles.module.scss";
 
 const ErrorPage = () => {
     return (
