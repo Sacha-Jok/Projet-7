9aa760d95f79a5a3ac331d7c60abb05b40077d5b
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-10 19:38:33 +0100

    add rating to rental page

--- src/pages/Rental/index.jsx
+++ src/pages/Rental/index.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Rentals from '../../datas/logements.json'
+import styles from "./styles.module.scss";
+
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
+import Dropdown from '../../components/Dropdown';
+import Tag from '../../components/Tag';
+import ErrorPage from '../ErrorPage';
+import Rating from '../../components/Rating';
 
 const Rental = () => {
+
+    const {id} = useParams();
+    const appart = Rentals.find(obj => obj.id === id);
+    const equipmentList = appart.equipments.map((equipment) => <li className={styles.__element} key={equipment}>{equipment}</li>);
+    
+    if (!appart) {
+        return(
+            <ErrorPage />
+        )
+    }
+
+    console.log(appart)
+
+
     return (
         <div>
             <Navbar />
-              Rental !
+                <div className={styles.__page}>
+                    <div className={styles.__carousel}> </div>
+                    <div className={styles.__appartHeader}>
+                        <h1 className={styles.__title}>{appart.title}</h1>
+                        <div className={styles.__host}>
+                            <h3 className={styles.__name}>{appart.host.name}</h3>
+                            <img className={styles.__pic} src={appart.host.picture} alt={appart.host.name} />
+                        </div>
+                    </div>
+                    <h2 className={styles.__subtitle}>{appart.location}</h2>
+                    <div className={styles.__stars}>
+                        <Tag tags={appart.tags} />
+                        <Rating rating={appart.rating}/>
+                    </div>
+                    <div className={styles.__dropdownContainer}>
+                        <Dropdown title='Description' text={appart.description} />
+                        <Dropdown title='Équipements' text={equipmentList} />
+                    </div>
+                </div>
             <Footer />
         </div>
     );
