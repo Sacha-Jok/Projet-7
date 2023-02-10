7d239b250a6afa031a4ae385886a67cf09d41453
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-10 04:20:55 +0100

    line height correction

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.scss";
 const Card = () => {
     return (
         Rentals.map(rentalInfo =>
-            <Link className={styles.__card} key={rentalInfo.id} to={`../logements/${rentalInfo.id}`} >
+            <Link className={styles.__card} key={rentalInfo.id} to={`../${rentalInfo.id}`} >
                     <div className={styles.__background}><img className={styles.__cover} src={rentalInfo.cover} alt={`Photo of  + ${rentalInfo.title}`} /></div>
                     <h2 className={styles.__title}>{rentalInfo.title}</h2>
             </Link>
