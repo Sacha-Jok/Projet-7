aaf01cfa796bf61c37a4705e9853e396e24ce77d
Sacha Malécot <sacha.malecot@gmail.com>
2023-02-11 17:00:19 +0100

    error page display correction

--- src/pages/Rental/index.jsx
+++ src/pages/Rental/index.jsx
@@ -14,34 +14,33 @@ import Lightbox from '../../components/Lightbox';
 const Rental = () => {
     const {id} = useParams();
     const appart = Rentals.find(obj => obj.id === id);
+  
+    if (!appart) return <ErrorPage />
+
     const equipmentList = appart.equipments.map((equipment) => <li className={styles.__element} key={equipment}>{equipment}</li>);
-    if (!appart) {
-        return(
-            <ErrorPage />
-        )
-    }
+
     return (
         <div>
             <Navbar />
-                <div className={styles.__page}>
-                    <Lightbox pictures={appart.pictures}/>
-                    <div className={styles.__appartHeader}>
-                        <h1 className={styles.__title}>{appart.title}</h1>
-                        <div className={styles.__host}>
-                            <h3 className={styles.__name}>{appart.host.name}</h3>
-                            <img className={styles.__pic} src={appart.host.picture} alt={appart.host.name} />
-                        </div>
-                    </div>
-                    <h2 className={styles.__subtitle}>{appart.location}</h2>
-                    <div className={styles.__stars}>
-                        <Tag tags={appart.tags} />
-                        <Rating rating={appart.rating}/>
-                    </div>
-                    <div className={styles.__dropdownContainer}>
-                        <Dropdown title='Description' text={appart.description} />
-                        <Dropdown title='Équipements' text={equipmentList} />
+            <div className={styles.__page}>
+                <Lightbox pictures={appart.pictures}/>
+                <div className={styles.__appartHeader}>
+                    <h1 className={styles.__title}>{appart.title}</h1>
+                    <div className={styles.__host}>
+                        <h3 className={styles.__name}>{appart.host.name}</h3>
+                        <img className={styles.__pic} src={appart.host.picture} alt={appart.host.name} />
                     </div>
                 </div>
+                <h2 className={styles.__subtitle}>{appart.location}</h2>
+                <div className={styles.__stars}>
+                    <Tag tags={appart.tags} />
+                    <Rating rating={appart.rating}/>
+                </div>
+                <div className={styles.__dropdownContainer}>
+                    <Dropdown title='Description' text={appart.description} />
+                    <Dropdown title='Équipements' text={equipmentList} />
+                </div>
+            </div>
             <Footer />
         </div>
     );
